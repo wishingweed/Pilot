669a17d0bffa26230b663a453f88857b7dd094cf
RavenKing <I077541@PVGN50900584A.apj.global.corp.sap>
2016-09-15 00:18:52 +0800

    Pilot post and get

--- Server/app/routes.js
+++ Server/app/routes.js
@@ -1,47 +1,30 @@
 // app/routes.js
-
-// grab the nerd model we just created
-var Nerd = require('./models/nerd');
-var Kevin = new Nerd({name:'Kevin'});
+var Pilot = require('./models/pilots')
 
     module.exports = function(app) {
-
-        // server routes ===========================================================
-        // handle things like api calls
-        // authentication routes
-
-        // sample api route
-        // app.all('*', function(req, res, next) {
-        //     res.header("Access-Control-Allow-Origin", "*");
-        //     // res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-        //     // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-        //     // res.header("X-Powered-By",' 3.2.1')
-        // });
-        app.get('/api/nerds', function(req, res) {
-            res.header("Access-Control-Allow-Origin", "*");
-            Kevin.save(function(err,silence)
-            {
-              if (err)
-                    res.send(err);
-            });
-            // use mongoose to get all nerds in the database
-            Nerd.find(function(err, nerds) {
+     app.get('/api/pilots', function(req, res) {
+            Pilot.find(function(err, pilots) {
                 // if there is an error retrieving, send the error. 
                                 // nothing after res.send(err) will execute
                 if (err)
                     res.send(err);
-                            console.log(nerds);
-                console.log(nerds);
-                res.json(nerds); // return all nerds in JSON format
+             console.log(pilots);
+                res.json(pilots); // return all nerds in JSON format
             });
 
         });
+      app.post('/api/pilots', function(req, res) {
+        console.log(req.body);
+        var newOne = new Pilot(req.body);
+        newOne.save(function(err){
+                if(err)
+                {    console.log(err);
+                    res.send(false);  
+                }
+        })
+        res.send(true);
 
-        // route to handle creating goes here (app.post)
-        // route to handle delete goes here (app.delete)
-
-        // frontend routes =========================================================
-        // route to handle all angular requests
+        });
         app.get('*', function(req, res) {
             res.sendfile('./public/views/index.html'); // load our public/index.html file
         });
