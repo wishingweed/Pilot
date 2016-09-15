d94eae3ef2102d62715a4669156ae399e62eebd5
RavenKing <I077541@PVGN50900584A.apj.global.corp.sap>
2016-09-15 21:26:11 +0800

    document get and post

--- Server/app/routes.js
+++ Server/app/routes.js
@@ -1,14 +1,39 @@
 // app/routes.js
 var Pilot = require('./models/pilots')
+var Document = require('./models/documents')
 
     module.exports = function(app) {
+         app.get('/api/documents', function(req, res) {
+            Document.find(function(err, documents) {
+                // if there is an error retrieving, send the error. 
+                                // nothing after res.send(err) will execute
+                if (err)
+                    res.send(err);
+                res.json(documents); // return all nerds in JSON format
+            });
+
+        });
+
+         app.post('/api/documents', function(req, res) {
+        console.log(req.body);
+        var newOne = new Document(req.body);
+        newOne.save(function(err){
+                if(err)
+                {  
+                   console.log(err);
+                    res.send(false);  
+                }
+        })
+        res.send(true);
+
+        });
+
      app.get('/api/pilots', function(req, res) {
             Pilot.find(function(err, pilots) {
                 // if there is an error retrieving, send the error. 
                                 // nothing after res.send(err) will execute
                 if (err)
                     res.send(err);
-             console.log(pilots);
                 res.json(pilots); // return all nerds in JSON format
             });
 
