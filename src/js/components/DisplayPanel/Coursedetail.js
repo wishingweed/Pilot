80a0673e2e8117c76835d7627b48076dbf602def
Yang <I077541@global.corp.sap>
2016-11-01 18:56:24 +0800

    good

--- src/js/components/DisplayPanel/Coursedetail.js
+++ src/js/components/DisplayPanel/Coursedetail.js
@@ -1,10 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Card,Icon,Timeline} from "antd";
+import {Card,Icon,Timeline,Button, Modal, Form, Input, Radio } from "antd";
 import {connect} from "react-redux"
 import { setCardDragable,handleFocus,setAreaDropable} from "../../interactScript";
 
 import {RemoveCard,ChangeStyle,ChangeToModify} from "../../Actions/pilotAction"
+
+
+
+const FormItem = Form.Item;
+
+const CollectionCreateForm = Form.create()(
+  (props) => {
+    const { visible, onCancel, onCreate, form } = props;
+    console.log(props);
+    const { getFieldDecorator } = form;
+    return (
+      <Modal
+        visible={visible}
+        title="Create a new collection"
+        okText="Create"
+        onCancel={onCancel}
+        onOk={onCreate}
+      >
+        <Form vertical>
+          <FormItem label="Title">
+            {getFieldDecorator('title', {
+              rules: [{ required: true, message: 'Please input the title of collection!' }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+          <FormItem label="Description">
+            {getFieldDecorator('description')(<Input type="textarea" />)}
+          </FormItem>
+          <FormItem className="collection-create-form_last-form-item">
+            {getFieldDecorator('modifier', {
+              initialValue: 'public',
+            })(
+              <Radio.Group>
+                <Radio value="public">Public</Radio>
+                <Radio value="private">Private</Radio>
+              </Radio.Group>
+            )}
+          </FormItem>
+        </Form>
+      </Modal>
+    );
+  }
+);
+
 
 @connect((store)=>{    
     return {
@@ -13,8 +58,35 @@ import {RemoveCard,ChangeStyle,ChangeToModify} from "../../Actions/pilotAction"
     };
     
 })
-export default class WorkFlowDetail extends React.Component {
+export default class CourseDetail extends React.Component {
+    constructor(props)
+    {
+      super(props)
 
+      this.state={ visible:false }
+
+    }
+    showModal() {
+    this.setState({ visible: true });
+  }
+  handleCancel() {
+    this.setState({ visible: false });
+  }
+  handleCreate() {
+    const form = this.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  }
+  saveFormRef(form) {
+    this.form = form;
+  }
 
 	componentDidMount(){
 
@@ -59,19 +131,23 @@ export default class WorkFlowDetail extends React.Component {
           return course;
         }
       });
-
-    console.log(targetdata);
-
+      const {details} = targetdata[0];
+      let timeline =  details.map((detail)=><Timeline.Item key={detail.id+"coursedetail"}>{detail.title}</Timeline.Item>)
 
         return (
         <div  class="workFlowDetailPanel">  
         <Card  title={targetdata[0].title} extra={<Icon type="cross" onClick={this.RemoveCard.bind(this)} />}>
         		<Timeline>
-			    <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
-			    <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
-			    <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
-			    <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+			    {timeline}
 				</Timeline>
+          <Button type="primary" onClick={this.showModal.bind(this)}>New Collection</Button>
+        <CollectionCreateForm
+          ref={this.saveFormRef.bind(this)}
+          visible={this.state.visible}
+          onCancel={this.handleCancel.bind(this)}
+          onCreate={this.handleCreate.bind(this)}
+        />
+        
 		</Card>
         </div>
       );
